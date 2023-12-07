777f6fd38590a5726879a326fc84110aae3bb476
Ashraful Islam <19103189@iubat.edu>
2023-12-08 04:29:07 +0600

    error handeling

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -2,21 +2,41 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
+import { ZodError } from "zod";
+import config from "../app/config";
+import handleZodError from "../errors/handleZodError";
+import { TErrorSources } from "../interface/error";
+import handleValidationError from "../errors/handleValidationError";
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Something went wrong!";
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let message = "Something went wrong!";
+  let errorSources: TErrorSources = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplyfiedError = handleZodError(err);
+    statusCode = simplyfiedError?.statusCode;
+    message = simplyfiedError?.message;
+    errorSources = simplyfiedError?.errorSources;
+  } else if (err?.name === "ValidationError") {
+    const simplyfiedError = handleValidationError(err);
+    statusCode = simplyfiedError?.statusCode;
+    message = simplyfiedError?.message;
+    errorSources = simplyfiedError?.errorSources;
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === "development" ? err?.stack : null,
+    // error: err,
   });
 };
 
